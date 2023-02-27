fcc8ed3778e2b160cfbcadbf5fbbd2e4b28c8325
Mustwin.Szczuply <patrox2005161@gmail.com>
2023-02-27 18:00:04 +0100

    Some Pretty useless updates
    added prev and next button, new css, functions made in diffrent way

--- index.js
+++ index.js
@@ -2,16 +2,10 @@ import BibliaEkumenicznaKsiegi from "./Jsons/BibliaEkumeniczna.json" assert { ty
 const BWBible = BibliaEkumenicznaKsiegi;
 const SelectBook = document.querySelector("#SelectBook");
 const SelectChapter= document.querySelector("#SelectChapters");
-
-// console.log(BWBible.BibliaEkumenicznaKsiegi[x].ROZDZIALY[y].WERSETY);
-
-// BWBible.BibliaEkumenicznaKsiegi[x].ROZDZIALY[y].WERSETY.forEach((werset) => {
-//   console.log(werset.TRESC);
-// });
-
-// BWBible.BibliaEkumenicznaKsiegi.forEach((x, index) => {
-//   console.log(`${index}: ${x.TYTUL} `);
-// });
+const prevChapterButton = document.getElementById('prevChapterButton');
+const nextChapterButton = document.getElementById('nextChapterButton');
+let currentChapter;
+let currentBook;
 
 
   BWBible.BibliaEkumenicznaKsiegi.forEach((x, index) => {
@@ -19,27 +13,44 @@ const SelectChapter= document.querySelector("#SelectChapters");
 });
 
 
-SelectBook.addEventListener("change", refreshChapters);
+SelectBook.addEventListener("change", ()=>{refreshChapters(SelectBook.value)});
+SelectChapter.addEventListener('change', ()=>{getVerses(SelectBook.value, SelectChapter.value)});
+nextChapterButton.addEventListener('click', ()=>{getVerses(SelectBook.value, (currentChapter+1))})
+prevChapterButton.addEventListener('click', ()=>{getVerses(SelectBook.value, (currentChapter-1))})
 
-SelectChapter.addEventListener('change', getVerses)
 
-function refreshChapters(){
+function refreshChapters(BookNumber){
+  currentBook = BookNumber;
   SelectChapter.innerHTML = ''
-   BWBible.BibliaEkumenicznaKsiegi[SelectBook.value].ROZDZIALY.forEach((x, index) => {
-    SelectChapter.innerHTML += `<option value=${index}> ${x.NR}</option>`
+   BWBible.BibliaEkumenicznaKsiegi[BookNumber].ROZDZIALY.forEach((x, index) => {
+    let newOption = document.createElement('option')
+    newOption.textContent = `${x.NR}`
+    newOption.setAttribute('value', `${index}`)
+    SelectChapter.appendChild(newOption)
 });
-getVerses()
+getVerses(SelectBook.value, SelectChapter.value)
 }
 
-function getVerses(){
-  document.querySelector('.content').innerHTML = ''
-  BWBible.BibliaEkumenicznaKsiegi[SelectBook.value].ROZDZIALY[SelectChapter.value].WERSETY.forEach((x, index) =>{
-      document.querySelector('.content').innerHTML +=  `<p class="Verse"><span class="VerseIndex">${index+1}</span> ${x.TRESC}</p>`;
-  })
+function getVerses(BookNumber, ChapterNumber){
+  if((ChapterNumber<0) || (ChapterNumber >= BWBible.BibliaEkumenicznaKsiegi[BookNumber].ROZDZIALY.length )){
+    alert('Przepraszamy, ale nie znaleźliśmy podanego rozdziału')
+  }
+  else{
+    document.querySelector('.content').innerHTML = ''
+    currentChapter = new Number(ChapterNumber);
+    BWBible.BibliaEkumenicznaKsiegi[BookNumber].ROZDZIALY[ChapterNumber].WERSETY.forEach((x, index) =>{
+      let newParagraph  = document.createElement('p');    
+      newParagraph.classList.add('verseParagraph')
+      newParagraph.innerHTML = `<span>${index+1}</span> ${x.TRESC}`;
+      document.querySelector('.content').appendChild(newParagraph)
+      console.log(BWBible.BibliaEkumenicznaKsiegi[BookNumber].ROZDZIALY.length)
+    })
+    console.log(ChapterNumber)
+  }
 }
 
-refreshChapters()
-getVerses()
+refreshChapters(SelectBook.value)
+getVerses(SelectBook.value, SelectChapter.value)
 
 
 
